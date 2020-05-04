76ce68bebc76e990e4de2cff2349c90b47769fb0
obsq <63944348+obsq@users.noreply.github.com>
2020-05-04 22:08:53 +0530

    Update backups.py

--- obsq/modules/backups.py
+++ obsq/modules/backups.py
@@ -7,21 +7,21 @@ from telegram import Message, Chat, Update, Bot
 from telegram.error import BadRequest
 from telegram.ext import CommandHandler, run_async, Filters
 
-import haruka.modules.sql.notes_sql as sql
-from haruka import dispatcher, LOGGER, OWNER_ID, SUDO_USERS, MESSAGE_DUMP
-from haruka.__main__ import DATA_IMPORT
-from haruka.modules.helper_funcs.chat_status import user_admin
-from haruka.modules.helper_funcs.misc import build_keyboard, revert_buttons
-from haruka.modules.helper_funcs.msg_types import get_note_type
-from haruka.modules.rules import get_rules
-import haruka.modules.sql.rules_sql as rulessql
-from haruka.modules.sql import warns_sql as warnssql
-import haruka.modules.sql.blacklist_sql as blacklistsql
-from haruka.modules.sql import disable_sql as disabledsql
-from haruka.modules.sql import cust_filters_sql as filtersql
-import haruka.modules.sql.welcome_sql as welcsql
-import haruka.modules.sql.locks_sql as locksql
-from haruka.modules.connection import connected
+import obsq.modules.sql.notes_sql as sql
+from obsq import dispatcher, LOGGER, OWNER_ID, SUDO_USERS, MESSAGE_DUMP
+from obsq.__main__ import DATA_IMPORT
+from obsq.modules.helper_funcs.chat_status import user_admin
+from obsq.modules.helper_funcs.misc import build_keyboard, revert_buttons
+from obsq.modules.helper_funcs.msg_types import get_note_type
+from obsq.modules.rules import get_rules
+import obsq.modules.sql.rules_sql as rulessql
+from obsq.modules.sql import warns_sql as warnssql
+import obsq.modules.sql.blacklist_sql as blacklistsql
+from obsq.modules.sql import disable_sql as disabledsql
+from obsq.modules.sql import cust_filters_sql as filtersql
+import obsq.modules.sql.welcome_sql as welcsql
+import obsq.modules.sql.locks_sql as locksql
+from obsq.modules.connection import connected
 
 @run_async
 @user_admin
